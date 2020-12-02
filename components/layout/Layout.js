e6f61e4c4ac2ee08194fd411ddcd5cd171e39e0a
jhon11390 <meljhon2@gmail.com>
2020-12-01 22:09:09 -0500

    add fonts google

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -1,19 +1,62 @@
 import React from 'react';
-import Link from 'next/link'
+import Header from './Header';
+import  { Global, css } from '@emotion/react';
+import Head from 'next/head';
 
 const Layout = props => {
-    return ( 
-        <>
-            <h1>Header</h1>
-            <nav>
-                <Link href="/">Inicio</Link>
-                <Link href="/nosotros">Nosotros</Link>
-            </nav>
-            <main>
-                {props.children}
-            </main>
-        </>
-     );
+  return ( 
+    <>
+			<Global 
+				styles={css`
+					:root {
+						--gris: #3d3d3d;
+						--gris2: #6F6F6F;
+						--naranja: #DA552F;
+					}
+
+					html {
+						font-size: 62.5%;
+						box-sizing: border-box;
+					}
+
+					*, *:before, *:after {
+						box-sizing: inherit;
+					}
+
+					body {
+						font-size: 1.6rem;
+						line-height: 1.5;
+					}
+
+					h1, h2, h3 {
+						margin: 0 0 2rem 0;
+						line-height: 1.5;
+					}
+
+					ul {
+						list-style: none;
+						margin: 0;
+						padding: 0;
+					}
+
+					a {
+						text-decoration: none;
+					}
+				`}
+			/>
+
+			<Head>
+				<html lang="es" />
+				<title>Product Hunt Firebase y Next.js</title>
+				<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w==" crossorigin="anonymous" />
+				<link href="https://fonts.googleapis.com/css?family=PT+Sans:400,700|Roboto+Slab:400,700&display=swap" rel="stylesheet" />
+			</Head>
+      <Header />  
+      <main>
+        {props.children}
+      </main>
+    </>
+   );
 }
  
 export default Layout;
